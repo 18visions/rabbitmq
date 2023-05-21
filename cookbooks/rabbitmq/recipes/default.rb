7054c20b5da542455b314d3d9934a6c66e295817
nick <nick.turner82@gmail.com>
2023-05-20 23:58:46 -0700

    cleanup recipe

--- cookbooks/rabbitmq/recipes/default.rb
+++ cookbooks/rabbitmq/recipes/default.rb
@@ -3,20 +3,6 @@
 # Recipe:: default
 #
 # Copyright:: 2023, The Authors, All Rights Reserved. 
-['htop','nano','python3'].each do |p|
-  package p do
-    action :install
-  end
-end
-
-yum_package 'logrotate' do
-  action :install
-end
-
-yum_package 'socat' do
-  action :install
-end
-
 execute 'rabbitmq signing key' do
   command 'rpm --import https://github.com/rabbitmq/signing-keys/releases/download/2.0/rabbitmq-release-signing-key.asc'
   action :run
@@ -44,6 +30,17 @@ execute 'yum update' do
   command 'sudo yum update -y'
 end
 
-yum_package 'rabbitmq-server' do
-  action :install
+#yum_package 'rabbitmq-server' do
+#  action :install
+#end
+
+['htop','nano','python3','logrotate','socat','rabbitmq-server'].each do |p|
+  package p do
+    action :install
+  end
 end
+
+service 'rabbitmq-server' do
+  action [:enable, :start]
+end
+
