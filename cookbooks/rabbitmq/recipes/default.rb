e6e6094f877a30965bf555a981ab6763d7e93e02
nick <nick.turner82@gmail.com>
2023-05-21 00:07:22 -0700

    consolidate packagers. enable rmq

--- cookbooks/rabbitmq/recipes/default.rb
+++ cookbooks/rabbitmq/recipes/default.rb
@@ -3,21 +3,6 @@
 # Recipe:: default
 #
 # Copyright:: 2023, The Authors, All Rights Reserved. 
-execute 'rabbitmq signing key' do
-  command 'rpm --import https://github.com/rabbitmq/signing-keys/releases/download/2.0/rabbitmq-release-signing-key.asc'
-  action :run
-end
-
-execute 'erlang repository gpg key' do
-  command 'rpm --import https://packagecloud.io/rabbitmq/erlang/gpgkey'
-  action :run
-end
-
-execute 'rmq repository gpg key' do
-  command 'rpm --import https://packagecloud.io/rabbitmq/rabbitmq-server/gpgkey'
-  action :run
-end
-
 cookbook_file '/etc/yum.repos.d/rabbitmq.repo' do
   source 'rabbitmq.repo'
   owner 'root'
@@ -30,10 +15,6 @@ execute 'yum update' do
   command 'sudo yum update -y'
 end
 
-#yum_package 'rabbitmq-server' do
-#  action :install
-#end
-
 ['htop','nano','python3','logrotate','socat','rabbitmq-server'].each do |p|
   package p do
     action :install
@@ -43,4 +24,3 @@ end
 service 'rabbitmq-server' do
   action [:enable, :start]
 end
-
